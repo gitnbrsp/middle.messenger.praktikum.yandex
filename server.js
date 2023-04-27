9300f992b653a12e83ea3be5523a89138763b6df
gitnbrsp <no@e-mail.com>
2023-04-27 08:51:06 +0300

    edit build script for netlify

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 const express = require("express");
-const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/dist"));
-
-app.get("/data", (req, res) => {
-    res.status(200).sendFile(__dirname + "/static/data.json");
-});
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
