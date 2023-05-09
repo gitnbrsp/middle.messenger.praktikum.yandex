066ef0700a19b89f3e04fa333ae81fcb52c3216e
gitnbrsp <no@e-mail.com>
2023-05-10 02:25:49 +0300

    Update express server

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.static(__dirname + "/dist"));
 
 app.get('*', function(req, res){
-    res.sendfile(__dirname + '/dist/index.html');
-});
+    const path = __dirname + '/dist/index.html'
+
+    if (fs.existsSync(path)) {
+        res.sendfile(path);
+    } else {
+        res.status(200).send('App not built yet');
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`app listening on port ${PORT}!`);
